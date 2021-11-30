d302b20afc9d4de4569cacddbd5563ec65ba8d8d
Jorge Montes <jamaff86@gmail.com>
2021-11-29 19:07:33 -0500

    added metod to query client

--- src/main/java/org/telegram/bot/client/IgdbClient.java
+++ src/main/java/org/telegram/bot/client/IgdbClient.java
@@ -1,13 +1,20 @@
 package org.telegram.bot.client;
 
+import com.api.igdb.exceptions.RequestException;
+import com.api.igdb.request.IGDBWrapper;
 import com.api.igdb.request.TwitchAuthenticator;
+import com.api.igdb.utils.Endpoints;
 import com.api.igdb.utils.TwitchToken;
+import com.google.protobuf.InvalidProtocolBufferException;
 import io.micronaut.context.annotation.Value;
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.discovery.event.ServiceReadyEvent;
+import proto.Game;
+import proto.GameResult;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class IgdbClient implements ApplicationEventListener<ServiceReadyEvent> {
@@ -18,12 +25,18 @@ public class IgdbClient implements ApplicationEventListener<ServiceReadyEvent> {
     @Value("${twitch.client.secret}")
     private String clientSecret;
 
+    public void jsonQuery() throws RequestException, InvalidProtocolBufferException {
+        var igdbWrapper = IGDBWrapper.INSTANCE;
+        byte[] bytes = igdbWrapper.apiProtoRequest(Endpoints.GAMES, "fields: *;");
+        List<Game> gamesList = GameResult.parseFrom(bytes).getGamesList();
+        gamesList.stream().forEach(System.out::println);
+
+    }
+
     @PostConstruct
     public void initialize() {
-        TwitchAuthenticator twitchAuthenticator = TwitchAuthenticator.INSTANCE;
-        TwitchToken twitchToken = twitchAuthenticator.requestTwitchToken(clientId, clientSecret);
-        String access_token = twitchToken.getAccess_token();
-        System.out.println("token: " + access_token);
+        IGDBWrapper igdbWrapper = IGDBWrapper.INSTANCE;
+        igdbWrapper.setCredentials(clientId, clientSecret);
     }
 
     @Override
--- src/main/java/org/telegram/bot/controller/BotController.java
+++ src/main/java/org/telegram/bot/controller/BotController.java
@@ -1,8 +1,11 @@
 package org.telegram.bot.controller;
 
+import com.api.igdb.exceptions.RequestException;
+import com.google.protobuf.InvalidProtocolBufferException;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import org.telegram.bot.IgdbWebhookBot;
+import org.telegram.bot.client.IgdbClient;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -16,12 +19,16 @@ public class BotController {
     @Inject
     private IgdbWebhookBot igdbWebhookBot;
 
+    @Inject
+    private IgdbClient igdbClient;
+
     @Post("/callback/")
     public Message getInfo(Message message) {
         System.out.println(message);
         try {
+            igdbClient.jsonQuery();
             igdbWebhookBot.execute(SendMessage.builder().chatId(message.getChatId().toString()).text(message.getText()).build());
-        } catch (TelegramApiException e) {
+        } catch (TelegramApiException | RequestException | InvalidProtocolBufferException e) {
             e.printStackTrace();
         }
         return message;
