dbf1ece95005f2898952edd7bff5e5568e37daf8
Jorge Montes <jamaff86@gmail.com>
2021-12-16 20:39:22 -0500

    added template an full respose

--- src/main/java/org/telegram/bot/controller/BotController.java
+++ src/main/java/org/telegram/bot/controller/BotController.java
@@ -1,7 +1,6 @@
 package org.telegram.bot.controller;
 
 import com.api.igdb.exceptions.RequestException;
-import com.google.common.collect.Lists;
 import com.google.protobuf.InvalidProtocolBufferException;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
@@ -9,17 +8,15 @@ import org.telegram.bot.IgdbWebhookBot;
 import org.telegram.bot.client.IgdbClient;
 import org.telegram.telegrambots.meta.api.methods.AnswerInlineQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.inlinequery.inputmessagecontent.InputMessageContent;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.inputmessagecontent.InputTextMessageContent;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResult;
 import org.telegram.telegrambots.meta.api.objects.inlinequery.result.InlineQueryResultArticle;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiValidationException;
 import proto.Game;
 
 import javax.inject.Inject;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller("/igdb")
@@ -44,13 +41,22 @@ public class BotController {
 
     private List<InlineQueryResult> mapToAnswerInlineQuery(List<Game> games) {
         return games.stream().map(game ->
-                InlineQueryResultArticle.builder().id(game.getId() + "").title(game.getName()).description(game.getSummary()).thumbUrl(getThumbUrl(game)).inputMessageContent(new InputTextMessageContent("lol")).build()
+                InlineQueryResultArticle.builder().id(game.getId() + "")
+                        .title(game.getName())
+                        .description(game.getSummary())
+                        .thumbUrl(getThumbUrl(game))
+                        .inputMessageContent(getMessageContent(game))
+                        .build()
         ).collect(Collectors.toList());
     }
 
+    private InputTextMessageContent getMessageContent(Game game) {
+        return InputTextMessageContent.builder().messageText("**lol**").parseMode("markdown").build();
+    }
+
     private String getThumbUrl(Game game) {
-        if(game.getArtworksCount() > 0)
-            return game.getArtworks(0).getUrl();
+        if (game.getArtworksCount() > 0)
+            return "https:" + game.getArtworks(0).getUrl();
         else
             return "";
     }
